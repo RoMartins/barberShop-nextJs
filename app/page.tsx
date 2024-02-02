120773925b3c114ec9cfb16beb55e9b2ddb584b9
Rodrigo <rodrigomartins_santos1804@hotmail.com>
2024-02-02 14:39:23 -0300

    feat: add welcome message in Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
-import { Header } from "./components/header";
+import { ptBR } from 'date-fns/locale'
+import { Header } from './components/header'
+import { format } from 'date-fns'
 
 export default function Home() {
-  return (
-     <>
-      <Header />
-     </>  
-  );
+    return (
+        <>
+            <Header />
+            <h2>Olá, Rodrigo!</h2>
+            <p className='capitalize'>{format(new Date, "EEEE ',' dd 'de' MMMM ", {
+              locale: ptBR
+            } )}</p>
+        </>  
+    )
 }
