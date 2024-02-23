db7ac96297076d00bf38734093670d5594e05678
Rodrigo <rodrigomartins_santos1804@hotmail.com>
2024-02-23 20:44:16 -0300

    feat: add service-item component

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { db } from '@/app/lib/prisma'
 import BarbershopInfo from '../_components/barbershop-info'
+import { Footer } from '@/app/components/ui/footer'
+import ServiceItem from '../_components/service-item'
 
 interface BarberShopsDetailParams {
   params: {
@@ -13,6 +15,9 @@ const BarberShopDetails = async ({params} : BarberShopsDetailParams) => {
     const barbershop = await db.barbershop.findUnique({
         where: {
             id: params.id
+        },
+        include: {
+            Service:true
         }
     })
 
@@ -21,7 +26,20 @@ const BarberShopDetails = async ({params} : BarberShopsDetailParams) => {
     }
 
     return (
-        <BarbershopInfo barbershop = {barbershop}/>
+        <>
+            <BarbershopInfo barbershop = {barbershop}/>
+            <div>
+                {barbershop.Service.map((service) => {
+                    return (
+                        <div className='flex flex-col px-5 mt-3'>
+                            <ServiceItem service={service}/>
+
+                        </div>
+                    )
+                })}                
+            </div>
+            <Footer />
+        </>
     )
 }
 
--- app/barbershops/_components/barbershop-info.tsx
+++ app/barbershops/_components/barbershop-info.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from '@/app/components/ui/button'
 import { Barbershop } from '@prisma/client'
-import { ChevronLeftIcon, MenuIcon } from 'lucide-react'
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
@@ -33,11 +33,16 @@ const BarbershopInfo = ( {barbershop} : BarberShopInfoProps) => {
                     }}/>
             </div>
 
-            <div className='px-5 py-3'>
+            <div className='px-5 py-3 border-b '>
                 <h1 className='text-xl font-bold'>{barbershop.name}</h1>
-                <div className="flex items-center gap-2">
-                
+                <div className='flex items-center gap-1 mt-2'>
+                    <MapPinIcon className='text-primary ' size={18}  />
                     <p  className='text-sm'>{barbershop.address}</p>
+                </div>
+
+                <div className="flex items-center gap-1 mt-2">
+                    <StarIcon className='text-primary ' size={18} />
+                    <p  className='text-sm'>5,0 (899 avaliações)</p>
                 </div>
             </div>
         </div>
