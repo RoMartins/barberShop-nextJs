93ab4f698f5c1ef7a6fe7c83254e678539763e1b
Rodrigo <rodrigomartins_santos1804@hotmail.com>
2024-03-18 12:52:07 -0300

    feat: add simenu component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import { Card, CardContent } from './ui/card'
 import { Button } from './ui/button'
 import { MenuIcon } from 'lucide-react'
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
+import SideMenu from './side-menu'
+
 
 
 export function Header() {
@@ -12,9 +15,21 @@ export function Header() {
         <Card>
             <CardContent className="py-6 px-5 flex justify-between items-center">
                 <Image src='/logo.png' alt="logo" width={120} height={22}/>
-                <Button>
-                    <MenuIcon size={16}/>
-                </Button>
+                <Sheet>
+                    <SheetTrigger>
+                        <Button variant='outline' size='icon' className='h-8 w-8'> 
+                            <MenuIcon size={16}/>
+                        </Button>
+                    </SheetTrigger>
+                    
+                    <SheetContent className='p-0'>
+                        <SheetHeader  className=' text-left border-b border-solid border-secondary p-5'>
+                            <SheetTitle>Menu</SheetTitle>
+                        </SheetHeader>
+                        <SideMenu />
+                    </SheetContent>
+                   
+                </Sheet>
             </CardContent>
         </Card>
     )
